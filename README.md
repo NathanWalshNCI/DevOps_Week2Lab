3ba03c1e1bd3bc362f0a6f5c615ba8b0a1cb2626
NathanWalshNCI <x22154086@student.ncirl.ie>
2023-02-01 15:35:02 +0000

    third commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Week 2 Lab – Bash & Git
+Nathan Walsh 22154086
+[Week 2 Lab](https://github.com/NathanWalshNCI/DevOps_Week2Lab.git)
+---
+To run the script.sh run: `bash script.sh`
--- script.sh
+++ script.sh
@@ -6,3 +6,9 @@ touch fol_2/2_1.txt, fol_2/2_2.txt fol_2/2_3.txt
 touch .gitignore
 echo "/fol_1" > .gitignore
 echo "/fol_2" >> .gitignore
+chmod 600 fol_1/1_1.txt
+chmod 600 fol_1/1_3.txt
+chmod 600 fol_2/2_1.txt
+chmod 600 fol_2/2_3.txt
+chmod 777 fol_1/1_2.txt
+chmod 777 fol_2/2_2.txt
